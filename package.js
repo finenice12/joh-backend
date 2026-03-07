f92f80ad04d7fb2bf7df4ab46d2fe58387f83776
finenice12 <omojegede90@gmail.com>
2026-03-07 14:43:58 +0100

    backend working with dotenv

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "joh-backend",
+  "version": "1.0.0",
+  "description": "JJ VTU Backend",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "dotenv": "^16.3.1",
+    "express": "^4.18.2"
+  }
+}
