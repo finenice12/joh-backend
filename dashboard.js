399560c2119e325b9664059ada6fbd73e6664c72
finenice12 <12345678+finenice12@users.noreply.github.com>
2026-03-17 05:37:54 +0100

    fix dashboard buttons working

---
+++ dashboard.js
@@ -0,0 +1,19 @@
+// FUND WALLET
+function fundWallet(){
+  alert("Fund wallet clicked");
+}
+
+// BUY AIRTIME
+function buyAirtime(){
+  alert("Buy airtime clicked");
+}
+
+// BUY DATA
+function buyData(){
+  alert("Buy data clicked");
+}
+
+// LOGOUT
+function logout(){
+  alert("Logout clicked");
+}
